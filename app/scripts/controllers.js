126df0c3d9bedfd527e6a241194381622768b935
Daniel Perez <dapehe94@gmail.com>
2014-12-04 22:27:21 +0000

    first commit

---
+++ app/scripts/controllers.js
@@ -0,0 +1,44 @@
+(function () {
+  'use strict';
+  
+  angular.module('blog.controllers', ['blog.services']);
+  
+    function PostListCtrl (Post) {
+      this.posts = Post.query();
+    }
+    
+    function PostDetailCtrl ($routeParams, Post, Comment, User) {
+      this.user = {};
+      this.post = {};
+      this.comments = {};
+
+      var self = this;
+
+      Post.query({ id: $routeParams.postId })
+        .$promise.then(
+          function (value) {
+            self.post = value[0];
+            self.user = User.query({ id: self.post.userId });
+          },
+          function (error) {
+            console.log('ERROR: ' + error);
+          }
+        );
+
+      this.comments = Comment.query({ postId: $routeParams.postId });
+
+    }
+
+    function PostCreateCtrl (Post) {
+      var self = this;
+      this.create = function() {
+        Post.save(self.post);
+      };
+    }
+
+    angular.module('blog.controllers')
+      .controller('PostListCtrl', PostListCtrl)
+      .controller('PostCreateCtrl', PostCreateCtrl)
+      .controller('PostDetailCtrl', PostDetailCtrl);    
+
+})();
